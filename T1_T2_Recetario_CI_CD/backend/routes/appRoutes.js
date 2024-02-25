857052f8a2794e4a01d1e77e977c2da777dc34e2
Josué Venegas Almonacid <josuevenegasalmonacid@gmail.com>
2024-02-25 12:53:01 +0100

    Console logs for PM2

--- T1_T2_Recetario_CI_CD/backend/routes/appRoutes.js
+++ T1_T2_Recetario_CI_CD/backend/routes/appRoutes.js
@@ -12,37 +12,65 @@ router.post("/create", (req, res, next) => {
   });
 
   newRecipe.save((error, recipe) => {
-    if (error) res.status(500).json({errmsg: error});
-    else res.status(200).json({msg: recipe});
+    if (error) {
+      console.log("[/create] Error: ", error);
+      res.status(500).json({errmsg: error});
+    }
+    else {
+      console.log("[/create] Success: ", recipe);
+      res.status(200).json({msg: recipe});
+    }
   });
   
 });
 
 router.get("/get", (req, res, next) => {
   Recipe.find({}, (error, recipes) => {
-    if (error) res.status(500).json({errmsg: error});
-    else res.status(200).json({msg: recipes});
+    if (error) {
+      console.log("[/get] Error: ", error);
+      res.status(500).json({errmsg: error});
+    }
+    else {
+      console.log("[/get] Success: ", recipes);
+      res.status(200).json({msg: recipes});
+    }
   });
 });
 
 router.get("/get/:id", (req, res, next) => {
   Recipe.findById(req.body._id, (error, recipe) => {
-    if (error) res.status(500).json({errmsg: error});
-    else res.status(200).json({msg: recipe});
+    if (error) {
+      console.log("[/get/:id] Error: ", error);
+      res.status(500).json({errmsg: error});
+    }
+    else {
+      console.log("[/get/:id] Success: ", recipe);
+      res.status(200).json({msg: recipe});
+    }
   });
 });
 
 router.put("/update", (req, res, next) => {
   Recipe.findById(req.body._id, (error, recipe) => {
-    if (error) res.status(500).json({errmsg: error});
+    if (error) {
+      console.log("[/update] Error: ", error);
+      res.status(500).json({errmsg: error});
+    }
     else{
       recipe.name =           req.body.name;
       recipe.ingredients =    req.body.ingredients;
       recipe.instructions =   req.body.instructions;
       recipe.image =          req.body.image;
+
       recipe.save((error, recipe) => {
-        if (error) res.status(500).json({errmsg: error});
-        else res.status(200).json({msg: recipe});
+        if (error) {
+          console.log("[/update] Error: ", error);
+          res.status(500).json({errmsg: error});
+        }
+        else {
+          console.log("[/update] Success: ", recipe);
+          res.status(200).json({msg: recipe});
+        }
       });
     }
   });
@@ -50,8 +78,14 @@ router.put("/update", (req, res, next) => {
 
 router.delete("/delete/:id", (req, res, next) => {
   Recipe.findOneAndRemove({_id: req.params.id}, (error, recipe) => {
-    if (error) res.status(500).json({errmsg: error});
-    else res.status(200).json({msg: recipe});
+    if (error) {
+      console.log("[/delete/:id] Error: ", error);
+      res.status(500).json({errmsg: error});
+    }
+    else {
+      console.log("[/delete/:id] Success: ", recipe);
+      res.status(200).json({msg: recipe});
+    }
   });
 });
 
